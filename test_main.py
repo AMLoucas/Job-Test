f3c89ef76c9ee31d3beb933dd7c4be6cd86baee7
Andreas Michalis Loucas <loucas.andrew97@hotmail.com>
2021-07-18 17:15:55 +0300

    The python test file being create

---
+++ test_main.py
@@ -0,0 +1,11 @@
+'''
+Test file that will apply Unit testing on methods of main.
+'''
+
+
+
+'''
+Main method condition that will call the appropriate functions to test the files.
+'''
+if __name__ == "__main__":
+    print("hello")
