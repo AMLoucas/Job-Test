f98ae5c1ef4f527a41c31e699268f075a1c3328c
Andreas Michalis Loucas <loucas.andrew97@hotmail.com>
2021-07-18 13:05:36 +0300

    Created Method that removes duplicates and converts id's to integers

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 Make sure all the packages are installed before trying to run the script.
 Importing libraries and packages that will be used in the program.
 """
-import csv
-
 import pandas
 
 
@@ -14,12 +12,35 @@ def read_csv_file(filename):
     return df
 
 
+def convert_user_to_int(csv_file):
+    # Sorting by userID
+    csv_file.sort_values("userId", inplace=True)
+    # Dropping columns not needed
+    csv_file = csv_file.drop(['itemId', 'rating', 'timestamp'], axis=1)
+    # Removing duplicates of users.
+    csv_file.drop_duplicates(subset="userId",
+                                  keep=False,
+                                  inplace=True)
+    # Resetting index to start from 0 and keep a correct sequence.
+    csv_file.reset_index(inplace=True,
+                         drop=True,
+                         level=0)
+    # Adding the new index (unique integer for each unique user) as column in datafram
+    csv_file['userIdAsInteger'] = csv_file.index
+    return csv_file
+
+
 """
 This is the main method.
 It is in charge of calling the correct process of the pipeline to accomplish the tasks in hand.
 """
 # Conditional to start the main process.
 if __name__ == '__main__':
+    # Read the CSV file.
     # Change the file path to suit your local directory.
     full_CSV_file = read_csv_file("/Users/louca5z/Downloads/xag.csv")
-    print(full_CSV_file)
+    # print(full_CSV_file)
+
+    # Converting the item and user ID's to integers.
+    userID_sorted = convert_user_to_int(full_CSV_file)
+    print(userID_sorted)
